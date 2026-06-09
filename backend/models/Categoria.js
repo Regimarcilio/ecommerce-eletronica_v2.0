1ab81d0b97fba03e70894753722c7fcdd7950bda
regimarcilio <reginaldo.marcilio@gmail.com>
2026-06-09 05:06:02 -0300

    reiciando do zero

--- backend/models/Categoria.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const categorySchema = new mongoose.Schema({
-    nome: String, icone: String, status: String, slug: String
-}, { timestamps: true });
-module.exports = mongoose.model('Categoria', categorySchema);
--- backend/models/Produto.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const productSchema = new mongoose.Schema({
-    nome: String, sku: String, preco: Number, quantidade: Number, status: String
-}, { timestamps: true });
-module.exports = mongoose.model('Produto', productSchema);
